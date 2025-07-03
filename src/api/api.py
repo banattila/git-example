35d0865b52cca10b19c5ae7f21d541668c4cd7d3
Attila Ban <banattila@protonmail.com>
2025-07-03 08:58:37 +0000

    Add argparser to api.py to reading the port and host

--- src/api/api.py
+++ src/api/api.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import argparse
 
 app = Flask(__name__)
 
@@ -7,4 +8,8 @@ def health():
     return "OK"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    parser = argparse.ArgumentParser(description="Example api starting")
+    parser.add_argument('--port', type=int, default=8080, help='Api port')
+    parser.add_argument('--host', type=str, default='localhost', help='Api host')
+    args = parser.parse_args()
+    app.run(host=args.host, port=args.port)
